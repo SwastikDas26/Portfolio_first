2109fbe1665f5aa2214ff517f0c90339cae1c843
Swastik <sr6015@srmist.edu.in>
2021-10-05 19:21:40 +0530

    first initialise

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        //making the navbar sticky after we scroll
+        //for making the navbar seen on top of the screen after we scroll down
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    // toggle menu/navbar script 
+    $('.menu-btn').click(function(){                    //I created this for the purpose of responsiveness
+        $('.navbar .menu').toggleClass("active");       
+        $('.menu-btn i').toggleClass("active");
+    });
+});
